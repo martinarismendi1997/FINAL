e542bcb2cb15de5fc481c068596dea5b581d25c1
Gustavo Falco <gustavo.falco1982@gmail.com>
2025-11-21 14:16:23 -0300

    acomode un poco el codigo

--- JS/codigo.js
+++ JS/codigo.js
@@ -6,7 +6,16 @@
 
         juegoSuma ()
 
-
+        document.getElementById("botonDatos").addEventListener("click", function() {
+        mostrarSeccion("datos");
+        });
+        document.getElementById("botonJuegos").addEventListener("click", function() {
+        mostrarSeccion("juegos");
+        });
+        document.getElementById("botonAdministrador").addEventListener("click", function() {
+        mostrarSeccion("admin");
+        });
+        mostrarSeccion("datos");
 
         document.getElementById("AgregarAlta").addEventListener("click",altaJugador)
 
@@ -42,8 +51,7 @@
             let op = document.createElement("option");
             op.value = nombre;
             op.textContent = nombre;
-            sel.appendChild(op);
-        
+            sel.appendChild(op);        
         }
 
         /*para resetear los campos de Alta Jugador, no me quedo con el reset*/
@@ -57,26 +65,24 @@
     let azarColsDif;
     let azarFilasDif;
 
-    function instertTot ()
-    { let tabla = document.getElementById("juegoDif");
-    tabla.innerHTML = ""
+    function instertTot (){ 
+        let tabla = document.getElementById("juegoDif");
+        tabla.innerHTML = ""
 
-    let azarPareja = Math.trunc(Math.random()*parejas.length); 
-    let azarFilas = Math.trunc(Math.random()*3+2);
-    let azarCols = Math.trunc(Math.random()*3+2);
-    let repetido = Math.trunc(Math.random()*2);
+        let azarPareja = Math.trunc(Math.random()*parejas.length); 
+        let azarFilas = Math.trunc(Math.random()*3+2);
+        let azarCols = Math.trunc(Math.random()*3+2);
+        let repetido = Math.trunc(Math.random()*2);
 
-    azarColsDif = Math.trunc(Math.random()*azarCols-1);
-    azarFilasDif = Math.trunc(Math.random()*azarFilas-1);
+        azarColsDif = Math.trunc(Math.random()*azarCols-1);
+        azarFilasDif = Math.trunc(Math.random()*azarFilas-1);
+        
+        AgregaATabla ( azarPareja,repetido, azarFilas, azarCols);
 
-    
-    AgregaATabla ( azarPareja,repetido, azarFilas, azarCols);
-
-    instertDif (azarFilasDif , azarColsDif, azarPareja, repetido );
+        instertDif (azarFilasDif , azarColsDif, azarPareja, repetido );
     }
 
-    function instertDif (filas, columnas, numeroPareja, emojin)
-    {
+    function instertDif (filas, columnas, numeroPareja, emojin){
         let otro = 1
         if (emojin == 1) {otro = 0}
 
@@ -85,69 +91,54 @@
     }
 
 
-    function AgregaATabla (conjunto, par, cantFil, CantCol)
-    {
+    function AgregaATabla (conjunto, par, cantFil, CantCol){
         let tabla = document.getElementById ("juegoDif")
-        for (let i=0 ; i<CantCol ; i++)
-            {
-                let fila = tabla.insertRow ();
-                
+        for (let i=0 ; i<CantCol ; i++){
+            let fila = tabla.insertRow ();                
 
-                for (let j=0 ; j<cantFil ; j++)
-                    {
-                        let cont = fila.insertCell ();
-                        cont.innerHTML = parejas [conjunto][par]
-                    }
-            } return tabla
-    }
-
-    
-    function clickear (evento)
-    {
-    let ResJuego1 = false 
-    let celda = evento.target.closest("td");
-    if (!celda) return;
-
-    let fila = celda.parentNode.rowIndex ;
-    let columna = celda.cellIndex ;
-    
-
-    if ((azarFilasDif == fila) && (azarColsDif == columna))
-           {
-            ResJuego1 = true
-            setTimeout(instertTot, 1000);
+            for (let j=0 ; j<cantFil ; j++){
+                let cont = fila.insertCell ();
+                cont.innerHTML = parejas [conjunto][par]
             }
-
-    if (ResJuego1 && tieneSonido()) {hacerSonido1()}
-
-    if (!ResJuego1 && tieneSonido()) {hacerSonido2()}
-
-    if (!tieneSonido() && ResJuego1) {alert("Correcto")}
+        } return tabla
+    }
     
-    if (!tieneSonido() && !ResJuego1) {alert("Incorrecto")}
+    function clickear (evento){
+        let ResJuego1 = false 
+        let celda = evento.target.closest("td");
+        if (!celda) return;
+
+        let fila = celda.parentNode.rowIndex ;
+        let columna = celda.cellIndex ;        
+
+        if ((azarFilasDif == fila) && (azarColsDif == columna)){
+                ResJuego1 = true
+                setTimeout(instertTot, 1000);}
+
+        if (ResJuego1 && tieneSonido()) {hacerSonido1()}
+
+        if (!ResJuego1 && tieneSonido()) {hacerSonido2()}
+
+        if (!tieneSonido() && ResJuego1) {alert("Correcto")}
+        
+        if (!tieneSonido() && !ResJuego1) {alert("Incorrecto")}
     };
 
-
-function tieneSonido () 
-{
-let check = document.getElementById("Sonido");
-let activado = check.checked
-
-return activado
+function tieneSonido (){
+    let check = document.getElementById("Sonido");
+    let activado = check.checked
+    return activado
 }
     
 function hacerSonido1() {
-  let sonido = new Audio("SONIDOS/ping.mp3");
-  sonido.play();
+    let sonido = new Audio("SONIDOS/ping.mp3");
+    sonido.play();
 }
 
 function hacerSonido2() {
-  let sonido = new Audio("SONIDOS/ping2.mp3");
-  sonido.play();
+    let sonido = new Audio("SONIDOS/ping2.mp3");
+    sonido.play();
 }
-
-
-
 
 let parejas = [
     ["🐶", "🐺"],
@@ -171,12 +162,9 @@ let parejas = [
     ["😐", "😶"],
 ]
 
-function comentario ()
-{
- let comentario = document.getElementById("Comentarios").value
-
+function comentario (){
+    let comentario = document.getElementById("Comentarios").value
 }
-
 
 
 /** -------------------------------------------- TERMINA JUEGO DE DIFERENCIA--------------------------------------------------------------**/
@@ -186,33 +174,34 @@ function comentario ()
 let num1 = 0
 let num2 = 0
 
-function juegoSuma ()
-{
- num1 = Math.trunc(Math.random()*10+1);
- num2 = Math.trunc(Math.random()*10+1);
+function juegoSuma (){
+    num1 = Math.trunc(Math.random()*10+1);
+    num2 = Math.trunc(Math.random()*10+1);
 
-let parrafo = document.getElementById("JuegoSuma")
-parrafo.innerHTML = ("Sume " + num1 + " + " + num2)
-
+    let parrafo = document.getElementById("JuegoSuma")
+    parrafo.innerHTML = ("Sume " + num1 + " + " + num2)
 }
 
-function ResultadoSuma () 
-{
- let resultado = false   
- let valor = document.getElementById("ResultSuma").value
- let valor1 = document.getElementById("ResultSuma")
- if (valor == (num1+num2)) {resultado = true; valor1.value = ""}
+function ResultadoSuma (){
+    let resultado = false   
+    let valor = document.getElementById("ResultSuma").value
+    let valor1 = document.getElementById("ResultSuma")
+    if (valor == (num1+num2)) {resultado = true; valor1.value = ""}
 
- if (resultado && tieneSonido()) 
-    {hacerSonido1(); setTimeout(juegoSuma,1000);valor1.style.backgroundColor = "lightgreen"}
+    if (resultado && tieneSonido()){
+        hacerSonido1(); setTimeout(juegoSuma,1000);valor1.style.backgroundColor = "lightgreen"
+    }
 
- if (!resultado && tieneSonido()) 
-    {hacerSonido2();valor1.style.backgroundColor = "yellow"}
+    if (!resultado && tieneSonido()){ 
+        hacerSonido2();valor1.style.backgroundColor = "yellow"
+    }
 
- if (resultado && !tieneSonido()) 
-    {setTimeout(juegoSuma,1000); ;valor1.style.backgroundColor = "lightgreen"}
+    if (resultado && !tieneSonido()){
+        setTimeout(juegoSuma,1000); ;valor1.style.backgroundColor = "lightgreen"
+    }
 
- if (!resultado && !tieneSonido()) 
-    {valor1.style.backgroundColor = "yellow"}
+    if (!resultado && !tieneSonido()){
+        valor1.style.backgroundColor = "yellow"
+    }
     
- }
+}
